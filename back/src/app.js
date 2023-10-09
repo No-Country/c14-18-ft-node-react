7dac212637890c045d26c818a7522a4dbe45427b
Alvaro <alvarord519@gmail.com>
2023-10-09 11:54:05 -0500

    adding DB connection

--- back/src/app.js
+++ back/src/app.js
@@ -1,18 +1,28 @@
 import express from 'express';
 import userRouter from './router/api.user.routes.js';
+import { sequelize } from './database/database.js'
 
 
-const app = express();
-const PORT = process.env.PORT || 8080;
+async function main() {
+    const app = express();
+    const PORT = process.env.PORT || 8080;
 
-app.use( express.urlencoded({extended:true}));
-
-
-app.get('/',(req,res)=>{
-    res.send('hola mundo')
-});
-
-app.use('/api/user',userRouter);
+    app.use(express.urlencoded({ extended: true }));
 
 
-app.listen(PORT,()=>console.log(`servidor escuchando en el puerto ${PORT}`));
+    app.get('/', (req, res) => {
+        res.send('hola mundo')
+    });
+
+    app.use('/api/user', userRouter);
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully')
+        app.listen(PORT, () => console.log(`servidor escuchando en el puerto ${PORT}`));
+
+    } catch (error) {
+        console.error('Unable to connect to the database:', error)
+    }
+}
+
+main()
