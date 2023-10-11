cc76717e1e7d106ba487f14072696d0b4ecd43f4
Eduardo Tolaba <eduardoetolaba@gmail.com>
2023-10-11 16:00:22 -0300

    cambios en backend, logica de login y agregado de variables de entorno

--- backend/src/controllers/loginControllers.js
+++ backend/src/controllers/loginControllers.js
@@ -1,25 +1,28 @@
 import jwt from 'jsonwebtoken';
 import { Patient } from '../models/user.js';
 import { compare } from 'bcrypt';
+import dotenv from 'dotenv';
+dotenv.config();
 
 
 export const login = async(req,res) =>{
     try {
-        const {email,password} = req.body;
-        if (!email || !password) return res.status(400).send({status:"error",message:"valores incompletos!"});
-        const user = await Patient.findOne({ where: {email:email}});//si existe el usuario me retorna los datos o sino null;
+        const {identityID,password} = req.body;
+
+        if (!identityID || !password) return res.status(400).send({status:"error",message:"valores incompletos!"});
+        const user = await Patient.findOne({ where: {documentId:identityID}});//si existe el usuario me retorna los datos o sino null;
         if (!user) return res.send({status:"error",error:"el usuario no esta registrado"});
         const passwordValidation = await compare(password, user.password);//comparo las contraseñas 
         if(passwordValidation === false) return res.send({status:"error",error:"la clave ingresada es incorrecta"});
-        //construyo el usuario con los valores que necesito
+        //construyo el usuario con los valores que necesito,para guarar en la cookie
         const userCredential = {
             name: user.name,
             identity: user.documentId
-        }
+        };
         //creo el token y guardo las credenciales del usuario.
-        const token = jwt.sign(userCredential,'secretToken2023',{expiresIn:"1d"});
+        const token = jwt.sign(userCredential,process.env.JWT_SECRET,{expiresIn:"1h"});
         //guardo el token en una cookie
-        res.cookie('cookiename',token,{sameSite:'none',secure:true}).send({status:"success",messages:"usuario logeado con exito!"});
+        res.cookie(process.env.JWT_NAME,token,{sameSite:'none',secure:true}).send({status:"success",messages:"usuario logeado con exito!"});
     } catch (error) {
         return res.status(500).send({status:"error",error:"Hubo un error en el servidor."});
     };
