1a1dddc94152fde024a06992d232aaf91a4de27c
Alvaro <alvarord519@gmail.com>
2023-10-12 00:03:27 -0500

    Changing landing page

--- frontend/src/app/(pages)/page.jsx
+++ frontend/src/app/(pages)/page.jsx
@@ -1,12 +1,34 @@
 import Link from 'next/link'
+import './landing.css'
 
 export default function Home() {
   return (
-    <main style={{height: '100vh', display: 'flex', flexDirection: 'column', placeItems: 'center'}}>
-      <h1>Landing Page</h1>
-      <Link href={'/sign-up'} style={{backgroundColor: '#4070f4', color: '#fff', padding: '10px 20px', borderRadius: '6px', textDecoration: 'none'}}>
-        Registrarse
-      </Link>
-    </main>
+    <>
+      <header className='landing-header'>
+        <h1>LOGO</h1>
+        <nav className='main-nav'>
+          <ul className='nav-items'>
+            <li>Home</li>
+            <li>Servicios</li>
+            <li>Medicos</li>
+            <li>Contacto</li>
+          </ul>
+        </nav>
+
+        <div>
+          <Link href={'/log-in'} style={{ backgroundColor: '#4070f4', color: '#fff', padding: '10px 20px', borderRadius: '6px', textDecoration: 'none' }}>
+            Ingresar
+          </Link>
+          <Link href={'/sign-up'} style={{ backgroundColor: '#4070f4', color: '#fff', padding: '10px 20px', borderRadius: '6px', textDecoration: 'none' }}>
+            Registrarse
+          </Link>
+        </div>
+      </header>
+
+      <main style={{display: 'flex', flexDirection: 'column', placeItems: 'center' }}>
+        <h1>Landing Page</h1>
+
+      </main>
+    </>
   )
 }
