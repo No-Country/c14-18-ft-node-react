dbfda30d6f1f1258dd3c605b370c20b8c4317e69
Alvaro <alvarord519@gmail.com>
2023-10-27 18:26:50 -0500

    Added citas card

--- frontend/src/app/(users)/myaccount/(routes)/historial-citas/page.jsx
+++ frontend/src/app/(users)/myaccount/(routes)/historial-citas/page.jsx
@@ -1,93 +1,80 @@
 'use client'
 
-import { useEffect, useState } from 'react';
-import  mockDoctors  from '@/mocks/doctors.json'
-import './historial-citas.css'
+import mockDoctors from "@/mocks/doctors.json";
+import "./historial-citas.css";
+import CitasCard from "@/components/ui/CitasCard/CitasCard";
+import { fetchCitas } from "@/utils/fetchCitas";
+import { useEffect, useState } from "react";
+import { format } from "date-fns";
+import { es } from "date-fns/locale";
 
 const HistorialCitas = () => {
 
     const [citas, setCitas] = useState([])
 
-    const userCredentials = sessionStorage.getItem('userCredentials')
+    const doctors = mockDoctors.doctors;
+    const userCredentials = sessionStorage.getItem("userCredentials");
     const parsedData = JSON.parse(userCredentials);
-    const userDocumentId = parsedData?.identity
-    const userName = parsedData?.name
-
-    const doctors = mockDoctors.doctors
-
-    const result = citas.map((cita) => {
-        const doctor = doctors.filter(item => item.id === cita.doctorId)
-        return doctor[0].name
-    })
-
-    console.log(result)
+    const userDocumentId = parsedData?.identity;
+    const userName = `${parsedData?.name} ${parsedData?.lastName}`
 
     useEffect(() => {
 
-        const userId = { documentId: userDocumentId }
-
-        const fetchCitas = async () => {
-
-            try {
-                const res = await fetch('http://localhost:8080/api/appointment/getAppointment', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    credentials: "include",
-                    body: JSON.stringify(userId),
-                })
-                
-                if( res.status === 200) {
-                    const data = await res.json()
-    
-                    setCitas(data.payload)
-                }
-            } catch (error) {
-                console.log(error)
-            }
-           
+        const fetch = async () => {
+            const res = await fetchCitas(userDocumentId)
+            const citasData = res.map(cita => {
+                const date = new Date(cita.date);
+                const formattedDate = format(date, 'EEE dd MMM p', {locale: es});
+                return { ...cita, date, formattedDate };
+              });
+          
+            setCitas(citasData);
         }
 
-        fetchCitas()
+        fetch()
+        
     }, [])
 
     return (
         <div className="historial__container">
             <div className="historial">
-                <header className='historial__header'>
-                    <h1 className='historial__title'>Historial de Citas</h1>
-                    <div className='historial__header__selections'>
-                        <div className='historial__header__selection'>
+                <header className="historial__header">
+                    <h1 className="historial__title">Historial de Citas</h1>
+                    <div className="historial__header__selections">
+                        <div className="historial__header__selection">
                             <span>Programadas</span>
                             <hr />
                         </div>
-                        <div className='historial__header__selection'>
+                        <div className="historial__header__selection">
                             <span>Historial</span>
                             <hr />
                         </div>
                     </div>
                 </header>
 
-                <main className='historial__content'>
-                    <div className='historial__content__grid'>
-                        {citas?.map(cita => {
-                            const doctor = doctors.filter(item => item.id === cita.doctorId)
+                <main className="historial__content">
+                    <div className="historial__content__grid">
+                        {citas?.map((cita) => {
+                            const doctor = doctors.filter(
+                                (item) => item.id === cita.doctorId
+                            );
 
-                            return (<div key={cita.id} className='historial__content__item'>
-                                <span>Dr {doctor[0].name}</span>
-                                <span>{doctor[0].specialty}</span>
-                                <span>{userName}</span>
-                            </div>)
+                            return (
 
-                        }
-                        )}
-
+                                <CitasCard
+                                    key={cita.id}
+                                    doctor={doctor[0].name}
+                                    specialty={doctor[0].specialty}
+                                    patient={userName}
+                                    date={cita.formattedDate}
+                                />
+                            );
+                        })}
                     </div>
                 </main>
             </div>
         </div>
     );
-}
+};
 
 export default HistorialCitas;
