4570998afeb65f3c875f24c9f249d82ae88152bc
Eduardo Tolaba <eduardoetolaba@gmail.com>
2023-10-31 18:34:15 -0300

    arreglos en el servidor, agregado de controlador,validacion de usuario logeado

--- backend/src/controllers/loginControllers.js
+++ backend/src/controllers/loginControllers.js
@@ -2,6 +2,7 @@ import jwt from 'jsonwebtoken';
 import { Patient } from '../models/user.js';
 import { compare } from 'bcrypt';
 import dotenvConfig from '../configs/dotenv.config.js';
+import UserDTO from '../DTO/user.dto.js';
 
 
 export const login = async (req, res) => {
@@ -13,12 +14,7 @@ export const login = async (req, res) => {
         if (!user) return res.status(400).send({ status: "error", error: "el DNI ingresado es incorrecto o no esta registrado" });
         const passwordValidation = await compare(password, user.password);//comparo las contraseñas 
         if (passwordValidation === false) return res.status(400).send({ status: "error", error: "la clave ingresada es incorrecta" });
-        //construyo el usuario con los valores que necesito,para guarar en la cookie
-        const userCredential = {
-            name: user.name,
-            lastName: user.last_name,
-            identity: user.documentId
-        };
+        const userCredential = UserDTO.getLoginUser(user);//retorna un objeto usuario con las propiedades necesarias.
         //creo el token y guardo las credenciales del usuario.
         const token = jwt.sign(userCredential, dotenvConfig.JWT.SECRET, { expiresIn: "1h" });
         //guardo el token en una cookie
