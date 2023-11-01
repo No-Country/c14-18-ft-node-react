28960a8a33d3393cfff458de0b52ea52cb7f9179
Alvaro <alvarord519@gmail.com>
2023-10-31 19:26:25 -0500

    Removed cookie validation on routes

--- frontend/src/app/(auth)/layout.jsx
+++ frontend/src/app/(auth)/layout.jsx
@@ -1,12 +1,6 @@
 import Navbar from "@/components/Navbar";
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
 
 export default function AuthLayout({ children }) {
-
-    const jwt = cookies().get("clinicaUser")?.value;
-
-    if(jwt) redirect('/')
 
     return (
         <>
