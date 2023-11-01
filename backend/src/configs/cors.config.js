79d0dfed91bc12506e793ac46f316c0a3040a3bc
Alvaro <alvarord519@gmail.com>
2023-10-31 20:21:14 -0500

    Changed Cors config

--- backend/src/configs/cors.config.js
+++ backend/src/configs/cors.config.js
@@ -3,7 +3,7 @@ dotenv.config();
 
 const corsOptions = {
     credentials: true,
-    origin: process.env.CORS_ORIGIN || "http://localhost:3000"
+    origin: [process.env.CORS_ORIGIN || "http://localhost:3000"]
 }
 
 export default corsOptions;
