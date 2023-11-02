a64a1f4cd759f2bd446f4789214199e05a2e04a1
Alvaro <alvarord519@gmail.com>
2023-11-01 20:31:59 -0500

    Fixed appointmentController

--- backend/src/controllers/appointmentController.js
+++ backend/src/controllers/appointmentController.js
@@ -27,9 +27,14 @@ export const CreateAppointment = async(req, res) => {
 export const GetAppointment = async(req, res) => {
     const user = await Patient.findOne({where: {documentId: req.body.documentId}})
 
-    console.log(`userId = ${user.dataValues.id}`)
+    try {
+        const appointments = await Appointments.findAll({where: {patientId: user?.dataValues.id}})
+        
+        res.status(200).send({payload:appointments})
+    } catch(e) {
+        console.log(e)
+        res.status(500).send('Hubo un error en el servidor')
+    }
 
-    const appointments = await Appointments.findAll({where: {patientId: user?.dataValues.id}})
 
-    res.status(200).send({payload:appointments})
 }
