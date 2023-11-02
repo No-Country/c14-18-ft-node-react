5a72c5693a09709b2e3ee41ebdcd10977d02dc11
Eduardo Tolaba <eduardoetolaba@gmail.com>
2023-11-02 16:47:51 -0300

    agregado de soluciones en el serviodr

--- backend/src/utils/dateCreate.js
+++
@@ -1,8 +0,0 @@
-
-export const dataCreate = () =>{
-    const año = new Date().getFullYear();
-    const mes = new Date().getMonth();
-    const dia = new Date().getDay();
-    const fecha = año+'-'+mes+'-'+dia;
-    return fecha;
-}
