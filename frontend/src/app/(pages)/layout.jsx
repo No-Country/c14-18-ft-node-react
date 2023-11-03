4ec78b2d2d9d74386e81d994cb377b89eb06ee6a
Alvaro <alvarord519@gmail.com>
2023-11-03 13:18:17 -0500

    Added Contact Form

--- frontend/src/app/(pages)/layout.jsx
+++ frontend/src/app/(pages)/layout.jsx
@@ -3,10 +3,10 @@ import Navbar from "@/components/NavBar/Navbar";
 
 export default function MainLayout({ children }) {
     return (
-        <div>
+        <>
             <Navbar/>
             { children }
             <Footer/>
-        </div>
+        </>
     )
 }
